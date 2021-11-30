cc143d3de9b11afa7ba84ed45a45c104ac206f37
Muhammadfajrulwafa07 <Muhammadfajrulwafatkj@gmail.com>
2021-11-30 11:17:48 +0800

    mengirim data menggunakan intent

--- app/src/main/java/com/muhammadfajrulwafa07/a211androidh/MainActivity.kt
+++ app/src/main/java/com/muhammadfajrulwafa07/a211androidh/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.muhammadfajrulwafa07.a211androidh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +23,22 @@ class MainActivity : AppCompatActivity() {
 
         simpanButton.setOnClickListener {
             val nama = namaEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val noHandphone = noHandphoneEditText.text.toString()
+            val tanggalLahir = tanggalLahirEditText.text.toString()
             var jarak = jarakEditText.text.toString().toInt()
 
             jarak = jarak * 1000
 
             Toast.makeText(this,"hai $jarak",Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this,HasilActivity::class.java)
+            intent.putExtra("nama",nama)
+            intent.putExtra("email",email)
+            intent.putExtra("noHandphone",noHandphone)
+            intent.putExtra("jarak",jarak)
+            intent.putExtra("tanggalLahir",tanggalLahir)
+            startActivity(intent)
         }
 
         batalButton.setOnClickListener {
